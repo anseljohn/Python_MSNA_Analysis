1bced94ea0f3662b64b343e818f28eb4cdeaffe5
John Anselmo <jjohnanselmo@gmail.com>
2024-02-01 12:38:57 -0600

    Add windows path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 An open-source program to analyze spontaneous sympathetic neurohemodynamic transduction
+
+https://pubmed.ncbi.nlm.nih.gov/33596745/
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 import adi
-f = adi.read_file(r'/Users/johnanselmo/Downloads/CSP_001_HUT_VNSOFF\ \(1\).adicht')
+# f = adi.read_file(r'/Users/johnanselmo/Downloads/CSP_001_HUT_VNSOFF\ \(1\).adicht')
+f = adi.read_file(r'/c/Users/Miguel Anselmo/Downloads/CSP_001_HUT_VNSOFF\ \(1\).adicht')

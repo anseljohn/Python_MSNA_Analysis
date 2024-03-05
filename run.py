9a7577fdf34f513f0c37a74432898eee7736169f
John Anselmo <jjohnanselmo@gmail.com>
2024-03-05 11:07:18 -0600

    Output to two files based on MAP or MDP outcome var

--- run.py
+++ run.py
@@ -3,27 +3,27 @@ import pandas as pd
 import transduction_analysis as ta
 
 # Check validity of call
-if (len(sys.argv) < 3):
-    sys.exit("Syntax Error:\n\t$ python analyze.py [path/to/spreadsheet.py] [outcome variable: MDP or MAP]")
+if (len(sys.argv) < 2):
+    sys.exit("Syntax Error:\n\t$ python analyze.py [path/to/spreadsheet.py]")
+
+########################
+#   Helper functions   #
+########################
+def fstr(f):
+    return str(round(f, 2))
 
 #######################
 #   Data formatting   #
 #######################
 df = pd.read_excel(sys.argv[1])
-df.drop([0, 1], inplace=True)           # Remove units and titles
-df.reset_index(drop=True, inplace=True) # Reset indexing back to 0
-ov = sys.argv[2]                        # The outcome variable, "MAP" or "MDP"
-
-# Check that outcome variable is valid
-if ov != "MDP" and ov != "MAP":
-    sys.exit("Syntax Error: Must choose between MAP (Mean Arterial Pressure) and MDP (Mean Diastolic Pressure) for outcome variable.\n\n\
-Example:\n\t$ python datapad_analysis.py path/to/spreadsheet.xlsx \"MDP\"")
+df.drop([0, 1], inplace=True)            # Remove units and titles
+df.reset_index(drop=True, inplace=True)  # Reset indexing back to 0
+outcome_variables = {"MDP": 5, "MAP": 6} # Outcome variables and their respective indices
 
 ############################################################
 #   Getting required arguments for analyses calculations   #
 ############################################################
 comnums = df.iloc[:, 4] # The burst comment numbers
-outcome = df.iloc[:, 5] if ov == "MDP" else df.iloc[:, 6] # MDP or MAP depending
 
 #TODO: Change this value to 7 once MAP is added into data pad
 burst_sizes = df.iloc[:, 6] # The burst size or "Integrated MSNA max-min"
@@ -50,27 +50,34 @@ for i in range(data_len):
         normalized_burst_amplitude_percent.append(None)
 
 # Creating the analyzer
-analyzer = ta.Transduction_Analysis(data_len, outcome, burst_checks, normalized_burst_amplitude_percent)
+for outcome_var in outcome_variables.keys():
+    outcome = df.iloc[:, outcome_variables[outcome_var]] if outcome_var == "MDP" else df.iloc[:, 5] #TODO: remove IF once MAP is added
+    analyzer = ta.Transduction_Analysis(data_len, outcome, burst_checks, normalized_burst_amplitude_percent)
 
-# Calculating and printing overall NVTD values
-overall_NVTD_values = analyzer.overall_NVTD()
-print("Max Overall Transduction Values:")
-print("\tAverage Absolute Change: " + str(overall_NVTD_values[0]))
-print("\tAverage Percent Change:  " + str(overall_NVTD_values[1]) + "\n")
+    overall_NVTD_values = analyzer.overall_NVTD()   # Calculating overall NVTD values
+    burst_pattern_values = analyzer.burst_pattern() # Calculating overall NVTD values per burst frequency
 
-# Calculating and printing overall NVTD values per burst frequency
-burst_pattern_values = analyzer.burst_pattern()
-print("Average Normalized Burst Sizes Per Sequence:")
+    # Writing everything to file
+    with open(outcome_var + '_transduction_analysis.txt', 'w') as f:
+        f.write("Transduction Analysis Using " + outcome_var + " as the Outcome Variable\n\n")
+        f.write("Max Overall Transduction Values:\n")
+        f.write("\tAverage Absolute Change: " + fstr(overall_NVTD_values[0]) + "\n")
+        f.write("\tAverage Percent Change: " + fstr(overall_NVTD_values[1]) + "\n\n")
 
-for i in range(4):
-    if i == 0:   print("\tSinglets:")
-    elif i == 1: print("\tDoublets:")
-    elif i == 2: print("\tTriplets:")
-    elif i == 3: print("\tOverall")
+        f.write("Average Normalized Burst Sizes Per Sequence:\n")
 
-    print("\t\tCount: " + str(burst_pattern_values[i][0]))
-    print("\t\tOverall NVTD: ", end="")
-    print(burst_pattern_values[i][1])
-    print("\t\tAverage Normalized Burst Amplitude: " + str(burst_pattern_values[i][2]))
-    print()g
+        for i in range(4):
+            if i == 0:   f.write("\tSinglets:\n")
+            elif i == 1: f.write("\tDoublets:\n")
+            elif i == 2: f.write("\tTriplets:\n")
+            elif i == 3: f.write("\tOverall:\n")
+
+            f.write("\t\tCount: " + fstr(burst_pattern_values[i][0]) + "\n")
+            f.write("\t\tOverall NVTD (12 cc): [")
+            bpv_NVTD = burst_pattern_values[i][1]
+            bpv_NVTD_len = len(bpv_NVTD)
+            for j in range(bpv_NVTD_len - 1):
+                f.write(fstr(bpv_NVTD[j]) + ", ")
+            f.write(fstr(bpv_NVTD[bpv_NVTD_len - 1]) + "]\n")
+            f.write("\t\tAverage Normalized Burst Amplitude: " + fstr(burst_pattern_values[i][2]) + "\n\n")
 
